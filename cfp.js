250bbf6f9fabe5326d8dfbadb5b4ffcc8aeef43a
channoh <channoh@gmail.com>
2016-04-04 21:25:31 +0900

    add CGO 2017

--- cfp.js
+++ cfp.js
@@ -9,5 +9,6 @@ var cfp = [
 ['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016'],
 ['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
 ['MICRO 2016', 'International Symposium on Microarchitecture', 'Oct 15, 2016 - Oct 19, 2016', 'Taiwan, Taipei', 'Apr 10, 2016 (Apr 3, 2016)'],
-['IEEE Micro IoT 2016', 'IEEE Micro Special Issue on Internet of Things (IoT)', '', '', 'Apr 29, 2016']
+['IEEE Micro IoT 2016', 'IEEE Micro Special Issue on Internet of Things (IoT)', '', '', 'Apr 29, 2016'],
+['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)']
 ];
