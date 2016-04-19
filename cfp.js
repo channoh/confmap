9d6bcb02cba9c4abf4483c16f92123ca7c5aa849
channoh <channoh@gmail.com>
2016-04-19 17:18:22 +0900

    update

--- cfp.js
+++ cfp.js
@@ -1,8 +1,6 @@
 var cfp = [
 ['ASPLOS 2016', '21st International Conference on Architectural Support for Programming Languages and Operating Systems', 'Apr 2, 2016 - Apr 6, 2016', 'Atlanta, GA, USA', 'Aug 12, 2015 (Aug 5, 2015)'],
-['HPCA 2016', 'The 22nd IEEE International Symposium on High Performance Computer Architecture (HPCA)', 'Mar 12, 2016 - Mar 16, 2016', 'Barcelona, Spain', 'Sep 11, 2015 (Sep 4, 2015)'],
 ['PPoPP 2016', 'Principle and Practice of Parallel Programming', 'Mar 12, 2016 - Mar 16, 2016', 'Barcelona, Spain', 'Sep 11, 2015 (Sep 4, 2015)'],
-['CGO 2016', 'International Symposium on Code Generation and Optimization', 'Mar 12, 2016 - Mar 18, 2016', 'Barcelona, Spain', 'Sep 18, 2015 (Sep 12, 2015)'],
 ['PLDI 2016', 'Programming Language Design and Implementation', 'Jun 13, 2016 - Jun 17, 2016', 'Santa Barbara, California, United States', 'Nov 20, 2015'],
 ['ISCA 2016', 'International Symposium on Computer Architecture', 'Jun 18, 2016 - Jun 22, 2016', 'Seoul, South Korea', 'Nov 23, 2015 (Nov 16, 2015)'],
 ['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 1, 2016'],
@@ -10,5 +8,6 @@ var cfp = [
 ['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
 ['MICRO 2016', 'International Symposium on Microarchitecture', 'Oct 15, 2016 - Oct 19, 2016', 'Taiwan, Taipei', 'Apr 10, 2016 (Apr 3, 2016)'],
 ['IEEE Micro IoT 2016', 'IEEE Micro Special Issue on Internet of Things (IoT)', '', '', 'Apr 29, 2016'],
+['HPCA 2017', 'The International Symposium on High-Performance Computer Architecture', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX, USA', 'Aug 1, 2016 (Jul 25, 2016)'],
 ['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)']
 ];
