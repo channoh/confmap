af96b73a15c8348c8fb0aa4ebc06d69d9847c5e1
channoh <channoh@gmail.com>
2016-06-27 16:37:26 +0900

    update recent data

--- cfp.js
+++ cfp.js
@@ -1,13 +1,11 @@
 var cfp = [
-['PLDI 2016', 'Programming Language Design and Implementation', 'Jun 13, 2016 - Jun 17, 2016', 'Santa Barbara, California, United States', 'Nov 20, 2015'],
 ['ISCA 2016', 'International Symposium on Computer Architecture', 'Jun 18, 2016 - Jun 22, 2016', 'Seoul, South Korea', 'Nov 23, 2015 (Nov 16, 2015)'],
-['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 1, 2016'],
-['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016'],
-['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
+['ASPLOS 2017', '22nd ACM International Conference on Architectural Support for Programming Languages and Operating Systems', 'Apr 8, 2017 - Apr 12, 2017', "Xi'an, China", 'Aug 15, 2016 (Aug 8, 2016)'],
 ['MICRO 2016', 'International Symposium on Microarchitecture', 'Oct 15, 2016 - Oct 19, 2016', 'Taiwan, Taipei', 'Apr 10, 2016 (Apr 3, 2016)'],
-['IEEE Micro IoT 2016', 'IEEE Micro Special Issue on Internet of Things (IoT)', '', '', 'Apr 29, 2016'],
 ['HPCA 2017', 'The International Symposium on High-Performance Computer Architecture', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX, USA', 'Aug 1, 2016 (Jul 25, 2016)'],
 ['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)'],
-['PPoPP 2017', 'Principle and Practice of Parallel Programming', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Aug 1, 2016 ()'],
-['ASPLOS 2017', '22nd International Conference on Architectural Support for Programming Languages and Operating Systems', 'Apr 8, 2017 - Apr 12, 2017', "Xi'an, China", 'Aug 15, 2016 (Aug 8, 2016)']
+['PLDI 2016', 'Programming Language Design and Implementation', 'Jun 13, 2016 - Jun 17, 2016', 'Santa Barbara, California, United States', 'Nov 20, 2015'],
+['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 8, 2016'],
+['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
+['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016']
 ];
