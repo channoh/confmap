7766331135eda61e5885566edd707197f9eb35ee
channoh <channoh@gmail.com>
2016-07-18 15:46:50 +0900

    PLDI 2017

--- cfp.js
+++ cfp.js
@@ -4,7 +4,7 @@ var cfp = [
 ['MICRO 2016', 'International Symposium on Microarchitecture', 'Oct 15, 2016 - Oct 19, 2016', 'Taiwan, Taipei', 'Apr 10, 2016 (Apr 3, 2016)'],
 ['HPCA 2017', 'The International Symposium on High-Performance Computer Architecture', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX, USA', 'Aug 1, 2016 (Jul 25, 2016)'],
 ['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)'],
-['PLDI 2016', 'Programming Language Design and Implementation', 'Jun 13, 2016 - Jun 17, 2016', 'Santa Barbara, California, United States', 'Nov 20, 2015'],
+['PLDI 2017', 'ACM SIGPLAN Symposium on Programming Language Design &Implementation;', 'Jun 19, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Nov 15, 2016'],
 ['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 8, 2016'],
 ['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
 ['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016']
