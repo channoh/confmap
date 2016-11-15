91f53358ef2f6a262f078591bdb9cfea9a19c520
channoh <channoh@gmail.com>
2016-11-15 20:20:39 +0900

    update PACT 2017

--- cfp.js
+++ cfp.js
@@ -6,6 +6,6 @@ var cfp = [
 ['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)'],
 ['PLDI 2017', 'ACM SIGPLAN Symposium on Programming Language Design & Implementation', 'Jun 19, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Nov 15, 2016'],
 ['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 8, 2016'],
-['PACT 2016', '25th International Conference on Parallel Architectures and Compilation Techniques', 'Sep 11, 2016 - Sep 15, 2016', 'Haifa, Israel', 'Mar 18, 2016 (Mar 12, 2016)'],
+['PACT 2017', 'International Conference on Parallel Architectures and Compilation Techniques', 'Sep 13, 2017 - Sep 17, 2017', 'Portland, Oregon, USA', 'Mar 14, 2017'],
 ['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016']
 ];
