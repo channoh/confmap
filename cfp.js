5a071ccf54a20f8f18efcfc4f021dbc15d2e0297
channoh <channoh@gmail.com>
2017-02-03 17:21:58 +0900

    update MICRO, LCTES, ISMM 2017

--- cfp.js
+++ cfp.js
@@ -1,11 +1,11 @@
 var cfp = [
 ['ISCA 2017', 'International Symposium on Computer Architecture', 'Jun 25, 2017 - Jun 28, 2017', 'Toronto, Canada', 'Nov 18, 2016 (Nov 11, 2016)'],
 ['ASPLOS 2017', '22nd ACM International Conference on Architectural Support for Programming Languages and Operating Systems', 'Apr 8, 2017 - Apr 12, 2017', "Xi'an, China", 'Aug 15, 2016 (Aug 8, 2016)'],
-['MICRO 2016', 'International Symposium on Microarchitecture', 'Oct 15, 2016 - Oct 19, 2016', 'Taiwan, Taipei', 'Apr 10, 2016 (Apr 3, 2016)'],
+['MICRO 2017', 'The 50th Annual IEEE/ACM International Symposium on Microarchitecture', 'Oct 14, 2017 - Oct 18, 2017', 'Boston', 'Apr 4, 2017 (Mar 28, 2017)'],
 ['HPCA 2017', 'The International Symposium on High-Performance Computer Architecture', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX, USA', 'Aug 1, 2016 (Jul 25, 2016)'],
 ['CGO 2017', 'International Symposium on Code Generation and Optimization', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX', 'Sep 9, 2016 (Sep 2, 2016)'],
 ['PLDI 2017', 'ACM SIGPLAN Symposium on Programming Language Design & Implementation', 'Jun 19, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Nov 15, 2016'],
-['LCTES 2016', 'ACM SIGPLAN/SIGBED Conference on Languages, Compilers, Tools and Theory for Embedded Systems', 'Jun 13, 2016 - Jun 14, 2016', 'Santa Barbara, California, USA', 'Feb 8, 2016'],
+['LCTES 2017', 'Languages, Compilers, and Tools for Embedded Systems', 'Jun 21, 2017 - Jun 22, 2017', 'Barcelona, Spain', 'Feb 14, 2017'],
 ['PACT 2017', 'International Conference on Parallel Architectures and Compilation Techniques', 'Sep 13, 2017 - Sep 17, 2017', 'Portland, Oregon, USA', 'Mar 14, 2017'],
-['ISMM 2016', 'The 2016 ACM SIGPLAN International Symposium on Memory Management', 'Jun 14, 2016 - Jun 14, 2016', 'Santa Barbara, California, United States', 'Feb 5, 2016']
+['ISMM 2017', '2017 ACM SIGPLAN International Symposium on Memory Management.', 'Jun 18, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Feb 26, 2017']
 ];
