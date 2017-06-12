6f0438c57dff8e1fd193ed99beb631b5f1344b3b
channoh <channoh@gmail.com>
2017-06-12 11:32:44 +0900

    hpca 2018

--- cfp.js
+++ cfp.js
@@ -2,7 +2,7 @@ var cfp = [
 ['ISCA 2017', 'International Symposium on Computer Architecture', 'Jun 25, 2017 - Jun 28, 2017', 'Toronto, Canada', 'Nov 18, 2016 (Nov 11, 2016)'],
 ['ASPLOS 2018', '23rd International Conference on Architectural Support for  Programming Languages and Operating Systems', 'Mar 24, 2018 - Mar 28, 2018', 'Williamsburg, VA, USA', 'Aug 11, 2017 (Aug 4, 2017)'],
 ['MICRO 2017', 'The 50th Annual IEEE/ACM International Symposium on Microarchitecture', 'Oct 14, 2017 - Oct 18, 2017', 'Boston', 'Apr 4, 2017 (Mar 28, 2017)'],
-['HPCA 2017', 'The International Symposium on High-Performance Computer Architecture', 'Feb 4, 2017 - Feb 8, 2017', 'Austin, TX, USA', 'Aug 1, 2016 (Jul 25, 2016)'],
+['HPCA 2018', 'HPCA 2018: 24th IEEE International Symposium on High-Performance Computer Architecture', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Aug 1, 2017 (Jul 25, 2017)'],
 ['CGO 2018', '2018 IEEE/ACM International Symposium on Code Generation and Optimization', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Sep 15, 2017 (Sep 8, 2017)'],
 ['PLDI 2017', 'ACM SIGPLAN Symposium on Programming Language Design & Implementation', 'Jun 19, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Nov 15, 2016'],
 ['LCTES 2017', 'Languages, Compilers, and Tools for Embedded Systems', 'Jun 21, 2017 - Jun 22, 2017', 'Barcelona, Spain', 'Feb 14, 2017'],
