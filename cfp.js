2c88cc13113bc3e6780e9ddee48665b4c0cb6494
channoh <channoh@gmail.com>
2017-12-05 15:31:39 +0900

    update

--- cfp.js
+++ cfp.js
@@ -1,11 +1,11 @@
 var cfp = [
-['ISCA 2018', 'International Symposium on Computer Architecture', 'Jun 2, 2018 - Jun 6, 2018', 'Los Angeles, California, USA', 'TBD'],
+['ISCA 2018', 'International Symposium on Computer Architecture', 'Jun 2, 2018 - Jun 6, 2018', 'Los Angeles, California, USA', 'Nov 21, 2017 (Nov 14, 2017)'],
 ['ASPLOS 2018', '23rd International Conference on Architectural Support for  Programming Languages and Operating Systems', 'Mar 24, 2018 - Mar 28, 2018', 'Williamsburg, VA, USA', 'Aug 11, 2017 (Aug 4, 2017)'],
 ['MICRO 2017', 'The 50th Annual IEEE/ACM International Symposium on Microarchitecture', 'Oct 14, 2017 - Oct 18, 2017', 'Boston', 'Apr 4, 2017 (Mar 28, 2017)'],
 ['HPCA 2018', 'HPCA 2018: 24th IEEE International Symposium on High-Performance Computer Architecture', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Aug 1, 2017 (Jul 25, 2017)'],
 ['CGO 2018', '2018 IEEE/ACM International Symposium on Code Generation and Optimization', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Sep 15, 2017 (Sep 8, 2017)'],
-['PLDI 2017', 'ACM SIGPLAN Symposium on Programming Language Design & Implementation', 'Jun 19, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Nov 15, 2016'],
+['PLDI 2018', 'Programming Language Design and Implementation', 'Jun 20, 2018 - Jun 22, 2018', 'Philadelphia', 'Nov 16, 2017'],
 ['LCTES 2017', 'Languages, Compilers, and Tools for Embedded Systems', 'Jun 21, 2017 - Jun 22, 2017', 'Barcelona, Spain', 'Feb 14, 2017'],
-['PACT 2017', 'International Conference on Parallel Architectures and Compilation Techniques', 'Sep 13, 2017 - Sep 17, 2017', 'Portland, Oregon, USA', 'Mar 14, 2017'],
+['PACT 2018', 'The 27th International Conference on Parallel and Compilation Techniques (PACT)', 'Sep 27, 2018 - Oct 1, 2018', 'Cyprus', 'TBD'],
 ['ISMM 2017', '2017 ACM SIGPLAN International Symposium on Memory Management.', 'Jun 18, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Mar 5, 2017']
 ];
