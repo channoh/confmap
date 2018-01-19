a7fc9013ae583ca9d1e72eb0b13f7391c5f4e9a8
channoh <channoh@gmail.com>
2018-01-19 16:41:59 +0900

    LCTES ISMM

--- cfp.js
+++ cfp.js
@@ -5,7 +5,7 @@ var cfp = [
 ['HPCA 2018', 'HPCA 2018: 24th IEEE International Symposium on High-Performance Computer Architecture', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Aug 1, 2017 (Jul 25, 2017)'],
 ['CGO 2018', '2018 IEEE/ACM International Symposium on Code Generation and Optimization', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Sep 15, 2017 (Sep 8, 2017)'],
 ['PLDI 2018', 'Programming Language Design and Implementation', 'Jun 20, 2018 - Jun 22, 2018', 'Philadelphia', 'Nov 16, 2017'],
-['LCTES 2017', 'Languages, Compilers, and Tools for Embedded Systems', 'Jun 21, 2017 - Jun 22, 2017', 'Barcelona, Spain', 'Feb 14, 2017'],
+['LCTES 2018', 'Languages, Compilers, and Tools for Embedded Systems', 'Jun 19, 2018 - Jun 20, 2018', 'Philadelphia, PA', 'Feb 23, 2018 (Feb 16, 2018)'],
 ['PACT 2018', 'The 27th International Conference on Parallel and Compilation Techniques (PACT)', 'Sep 27, 2018 - Oct 1, 2018', 'Cyprus', 'TBD'],
-['ISMM 2017', '2017 ACM SIGPLAN International Symposium on Memory Management.', 'Jun 18, 2017 - Jun 23, 2017', 'Barcelona, Spain', 'Mar 5, 2017']
+['ISMM 2018', 'International Symposium on Memory Management', 'Jun 18, 2018 - Jun 18, 2018', 'Philadelphia, Pennsylvania, USA', 'Feb 15, 2018']
 ];
