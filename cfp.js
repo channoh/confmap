f83890f0a43af88d7c9d153c61e0dc91c4af648a
channoh <channoh@gmail.com>
2018-03-19 17:49:11 +0900

    MICRO 2018

--- cfp.js
+++ cfp.js
@@ -1,7 +1,7 @@
 var cfp = [
 ['ISCA 2018', 'International Symposium on Computer Architecture', 'Jun 2, 2018 - Jun 6, 2018', 'Los Angeles, California, USA', 'Nov 21, 2017 (Nov 14, 2017)'],
 ['ASPLOS 2018', '23rd International Conference on Architectural Support for  Programming Languages and Operating Systems', 'Mar 24, 2018 - Mar 28, 2018', 'Williamsburg, VA, USA', 'Aug 11, 2017 (Aug 4, 2017)'],
-['MICRO 2017', 'The 50th Annual IEEE/ACM International Symposium on Microarchitecture', 'Oct 14, 2017 - Oct 18, 2017', 'Boston', 'Apr 4, 2017 (Mar 28, 2017)'],
+['MICRO 2018', 'The 51st Annual IEEE/ACM International Symposium on Microarchitecture', 'Oct 20, 2018 - Oct 24, 2018', 'Fukuoka, Japan', 'Apr 6, 2018 (Mar 30, 2018)'],
 ['HPCA 2018', 'HPCA 2018: 24th IEEE International Symposium on High-Performance Computer Architecture', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Aug 1, 2017 (Jul 25, 2017)'],
 ['CGO 2018', '2018 IEEE/ACM International Symposium on Code Generation and Optimization', 'Feb 24, 2018 - Feb 28, 2018', 'Vienna, Austria', 'Sep 15, 2017 (Sep 8, 2017)'],
 ['PLDI 2018', 'Programming Language Design and Implementation', 'Jun 20, 2018 - Jun 22, 2018', 'Philadelphia', 'Nov 16, 2017'],
